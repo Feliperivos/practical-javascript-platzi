ce30aeb1318836d7a8a2e143dea74ae615a133f9
feliperivos <feliperivos@gmail.com>
2021-09-24 07:18:17 -0500

    Connected to HTML

--- descuentos.js
+++ descuentos.js
@@ -5,3 +5,10 @@ function calcularPrecioConDescuento(precio, descuento){
 }
 
 
+function onClickButtonPriceDiscount(){
+    const precio = document.getElementById("InputPrice").value;
+    const descuento = document.getElementById("InputDiscount").value;
+    const precioConDescuento = calcularPrecioConDescuento(precio, descuento);
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento es: " + precioConDescuento;
+}
